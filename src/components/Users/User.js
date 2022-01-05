2c9ed0ecd2f0853b7d1fea274b5e17e3aea4cbeb
Kawser Ahmed Shovon <ahmedshovoncse13@gmail.com>
2022-01-05 22:06:50 +0600

    made users UI

--- src/components/Users/User.js
+++ src/components/Users/User.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import "./User.css"
 
 const User = ({ user }) => {
-    const { name, img } = user
+    const { name, img, phone, address } = user
     return (
         <div className='col'>
-            <div className="card">
-                <img src={img} alt="" className='img-fluid' />
-                <h5>{name}</h5>
+            <div className="card h-100 shadow-sm">
+                <div className="d-flex p-2 pb-xl-0">
+                    <img src={img} alt="" className='img-fluid user-img' />
+                    <div className="p-2">
+                        <h6 className="mb-0 fw-light"><span className="fw-bold text-secondary">Name: </span>{name}</h6>
+                        <p className="mb-0 fw-light"><span className="fw-bold text-secondary">Phone: </span> {phone}</p>
+                        <p className="mb-0 fw-light"><span className="fw-bold text-secondary">Address: </span>House: {address.house}, {address.city}-{address.pinCode}</p>
+                    </div>
+                </div>
             </div>
         </div>
     );
--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -10,7 +10,7 @@ const Users = () => {
     }, [])
     return (
         <div className='container my-5'>
-            <div className="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4">
+            <div className="row row-cols-1 row-cols-md-2 row-cols-xl-3 g-4">
                 {users.map(user => <User user={user} key={user._id} />)}
             </div>
         </div>
