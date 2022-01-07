6de296ed478da09212b6abb721ad38d189631a05
Kawser Ahmed Shovon <ahmedshovoncse13@gmail.com>
2022-01-07 21:01:11 +0600

    updated user card

--- src/components/Users/User.js
+++ src/components/Users/User.js
@@ -6,15 +6,17 @@ const User = ({ user }) => {
     return (
         <div className='col'>
             <div className="card h-100 shadow-sm user-card">
-                <div className="d-flex d-xl-block align-items-center p-2">
-                    <div className="text-center mb-xl-3">
+                <div className="d-flex d-md-block d-lg-flex d-xl-block align-items-center p-2">
+                    <div className="text-center mb-md-3 mb-lg-0 mb-xl-3">
                         <img src={img} alt="" className='img-fluid user-img rounded-circle' />
                     </div>
                     <div className="px-2">
-                        <h6 className="mb-0 fw-light"><span className="fw-bold text-secondary">Name: </span>{name}</h6>
+                        <h6 className="mb-0"><span className="fw-bold text-secondary">Name: </span>{name}</h6>
                         <p className="fw-light"><span className="fw-bold text-secondary">Phone: </span> {phone}</p>
                         <p className="fw-light"><span className="fw-bold text-secondary">Address: </span>House: {address.house}, {address.city}-{address.pinCode}</p>
-                        <p className='text-success fw-bold'>{orders.length} {orders.length > 1 ? "Orders" : "Order"}</p>
+                        <div className="border-top pt-1 mt-1">
+                            <p className='bg-success badge rounded-pill'>{orders.length} {orders.length > 1 ? "Orders" : "Order"}</p>
+                        </div>
                     </div>
                 </div>
             </div>
--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -10,7 +10,7 @@ const Users = () => {
     }, [])
     return (
         <div className='container my-5'>
-            <div className="row row-cols-1 row-cols-lg-2 row-cols-xl-4 g-4">
+            <div className="row row-cols-1 row-cols-md-2 row-cols-xl-4 g-4">
                 {users.map(user => <User user={user} key={user._id} />)}
             </div>
         </div>
