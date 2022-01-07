a297b0f3b4ec6e6175bccfdb32edcc7b9b70bf83
Kawser Ahmed Shovon <ahmedshovoncse13@gmail.com>
2022-01-07 22:20:31 +0600

    updated navbar buttons

--- src/shared/Header/Header.js
+++ src/shared/Header/Header.js
@@ -12,28 +12,26 @@ const Header = () => {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <NavLink style={({ isActive }) => ({ color: isActive ? '#FC9219' : 'gray' })} to="/home" className="nav-link" aria-current="page" >Home</NavLink>
+                            <NavLink style={({ isActive }) => ({ color: isActive ? '#FC9219' : 'gray', fontWeight: isActive ? '700' : '400' })} to="/home" className="nav-link" aria-current="page" >Home</NavLink>
                         </li>
                         <li className="nav-item">
-                            <NavLink style={({ isActive }) => ({
-                                color: isActive ? '#FC9219' : 'gray',
-                            })} to="/products" className="nav-link" aria-current="page" >Products</NavLink>
+                            <NavLink style={({ isActive }) => ({ color: isActive ? '#FC9219' : 'gray', fontWeight: isActive ? '700' : '400' })} to="/products" className="nav-link" aria-current="page" >Products</NavLink>
                         </li>
                         <li className="nav-item">
-                            <NavLink style={({ isActive }) => ({ color: isActive ? '#FC9219' : 'gray' })} to="/users" className="nav-link" aria-current="page" >Users</NavLink>
+                            <NavLink style={({ isActive }) => ({ color: isActive ? '#FC9219' : 'gray', fontWeight: isActive ? '700' : '400' })} to="/users" className="nav-link" aria-current="page" >Users</NavLink>
                         </li>
                         <li className="nav-item">
-                            <NavLink style={({ isActive }) => ({ color: isActive ? '#FC9219' : 'gray' })} to="/orders" className="nav-link">
+                            <NavLink style={({ isActive }) => ({ color: isActive ? '#FC9219' : 'gray', fontWeight: isActive ? '700' : '400' })} to="/orders" className="nav-link">
                                 Orders
                             </NavLink>
                         </li>
                     </ul>
                     <div className="d-flex justify-content-center">
                         <Link to="/signup" >
-                            <div className="btn btn-sm btn-primary me-2">Sign Up</div>
+                            <div className="btn signup-btn me-2">Sign Up</div>
                         </Link>
                         <Link to="/login" >
-                            <div className="btn btn-sm btn-outline-primary">Log In</div>
+                            <div className="btn login-btn">Log In</div>
                         </Link>
                     </div>
                 </div>
