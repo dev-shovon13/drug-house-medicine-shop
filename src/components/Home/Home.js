15fd5b3d878aeda02c6c731f9a856aa0c338f722
Kawser Ahmed Shovon <ahmedshovoncse13@gmail.com>
2022-01-09 02:02:31 +0600

    added why choose us part on home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,6 @@
 import { Helmet } from 'react-helmet';
 import Banner from '../Banner/Banner';
+import WhyUs from '../WhyUs/WhyUs';
 
 const Home = () => {
     return (
@@ -9,6 +10,7 @@ const Home = () => {
                 <meta name="This is the home page of Drug House" content="Drug House- online medicine shop Website" />
             </Helmet>
             <Banner/>
+            <WhyUs/>
         </>
     );
 };
---
+++ src/components/WhyUs/WhyUs.js
@@ -0,0 +1,17 @@
+
+const WhyUs = () => {
+    return (
+        <div className="bg-white">
+            <div className="container">
+                <div className="text-center py-4">
+                    <h2 className="mb-0 title bg-light">Why Choose Us</h2>
+                    <hr className="mt-0" />
+                    <p className="text-secondary w-75 mx-auto">We have a rich history of helping improve the lives of many not only through our pharmacy services, but also through our commitment to giving back to our communities. We have always been dedicated to the caring for the health and well-being of our employees, patients and customers, as well as the communities in which they live and work.</p>
+                </div>
+                <img src="https://i.ibb.co/RzgLVsh/why-us.png" alt="" className="img-fluid" />
+            </div>
+        </div>
+    );
+};
+
+export default WhyUs;
